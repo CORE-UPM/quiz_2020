9d9756cef8221bc04290437766af68eba1886148
Santiago Pavon <santiago@dit.upm.es>
2019-07-08 13:10:05 +0200

    Step 1.3: Application Layout.

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ var path = require('path');
 var favicon = require('serve-favicon');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var partials = require('express-partials');
 
 var indexRouter = require('./routes/index');
 
@@ -19,6 +20,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
+app.use(partials());
 
 app.use('/', indexRouter);
 
