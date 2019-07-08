4ab9e900babc99a12506412f7a87ec48610cda4a
Santiago Pavon <santiago@dit.upm.es>
2019-07-08 13:01:11 +0200

    Step 1.2.4: Add Author Page.

--- routes/index.js
+++ routes/index.js
@@ -6,4 +6,9 @@ router.get('/', function(req, res, next) {
   res.render('index');
 });
 
+// Author page.
+router.get('/author', (req, res, next) => {
+  res.render('author');
+});
+
 module.exports = router;
