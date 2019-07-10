7573fce67aaf66aa5736cc7d846153b67c8e9f39
Santiago Pavon <santiago@dit.upm.es>
2019-07-10 12:06:18 +0200

    Step 2.2: Pagination.

--- controllers/quiz.js
+++ controllers/quiz.js
@@ -1,6 +1,8 @@
 const Sequelize = require("sequelize");
 const {models} = require("../models");
 
+const paginate = require('../helpers/paginate').paginate;
+
 // Autoload el quiz asociado a :quizId
 exports.load = async (req, res, next, quizId) => {
 
@@ -22,7 +24,25 @@ exports.load = async (req, res, next, quizId) => {
 exports.index = async (req, res, next) => {
 
     try {
-        const quizzes = await models.Quiz.findAll();
+        const count = await models.Quiz.count();
+
+        // Pagination:
+
+        const items_per_page = 10;
+
+        // The page to show is given in the query
+        const pageno = parseInt(req.query.pageno) || 1;
+
+        // Create a String with the HTMl used to render the pagination buttons.
+        // This String is added to a local variable of res, which is used into the application layout file.
+        res.locals.paginate_control = paginate(count, items_per_page, pageno, req.url);
+
+        const findOptions = {
+            offset: items_per_page * (pageno - 1),
+            limit: items_per_page
+        };
+
+        const quizzes = await models.Quiz.findAll(findOptions);
         res.render('quizzes/index.ejs', {quizzes});
     } catch (error) {
         next(error);
