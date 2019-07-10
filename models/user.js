f6e7aa80fae8e742a9a27f7cdc49628e2ab8f703
Santiago Pavon <santiago@dit.upm.es>
2019-07-10 16:09:55 +0200

    Step 4.1: Users Resource.

---
+++ models/user.js
@@ -0,0 +1,84 @@
+const {Model} = require('sequelize');
+const crypt = require('../helpers/crypt');
+
+// Definition of the User model:
+
+module.exports = function (sequelize, DataTypes) {
+
+    // Account types.
+    // Account type ID is the index in this array.
+    let accountTypes = [
+        "local",
+        "github",
+        "twitter",
+        "google",
+        "facebook",
+        "linkedin"
+    ];
+
+    class User extends Model {
+
+        // Returns the ID of the given account type.
+        // AccountTypeId of local accounts is 0.
+        static accountTypeId(name) {
+            return accountTypes.indexOf(name);
+        }
+
+        get displayName() {
+            if (!this.accountTypeId) {
+                return `${this.username} (${accountTypes[0]})`;
+            } else {
+                return `${this.profileName} (${accountTypes[this.accountTypeId]})`;
+            }
+        }
+    }
+
+    User.init({
+            username: {
+                type: DataTypes.STRING,
+                unique: true,
+                validate: {notEmpty: {msg: "Username must not be empty."}}
+            },
+            password: {
+                type: DataTypes.STRING,
+                validate: {notEmpty: {msg: "Password must not be empty."}},
+                set(password) {
+                    // Random String used as salt.
+                    this.salt = Math.round((new Date().valueOf() * Math.random())) + '';
+                    this.setDataValue('password', crypt.encryptPassword(password, this.salt));
+                }
+            },
+            salt: {
+                type: DataTypes.STRING
+            },
+            isAdmin: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
+            },
+            accountTypeId: {
+                type: DataTypes.INTEGER,
+                unique: "profileUniqueValue",
+                default: 0,
+                validate: {
+                    min: {
+                        args: [0],
+                        msg: "ProfileId must be positive."
+                    }
+                }
+            },
+            profileId: {
+                type: DataTypes.INTEGER,
+                unique: "profileUniqueValue",
+                validate: {notEmpty: {msg: "accountTypeId must not be empty."}}
+            },
+            profileName: {
+                type: DataTypes.STRING,
+                validate: {notEmpty: {msg: "ProfileName must not be empty."}}
+            }
+        }, {
+            sequelize
+        }
+    );
+
+    return User;
+};
--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,7 @@ var express = require('express');
 var router = express.Router();
 
 const quizController = require('../controllers/quiz');
+const userController = require('../controllers/user');
 
 //-----------------------------------------------------------
 
@@ -26,9 +27,10 @@ function saveBack(req, res, next) {
 //   /new, /edit, /play, /check, or /:id.
 router.get(
     [
-      '/',
-      '/author',
-      '/quizzes'
+        '/',
+        '/author',
+        '/users',
+        '/quizzes'
     ],
     saveBack);
 
@@ -47,6 +49,17 @@ router.get('/author', (req, res, next) => {
 
 // Autoload for routes using :quizId
 router.param('quizId', quizController.load);
+router.param('userId', userController.load);
+
+
+// Routes for the resource /users
+router.get('/users',                    userController.index);
+router.get('/users/:userId(\\d+)',      userController.show);
+router.get('/users/new',                userController.new);
+router.post('/users',                   userController.create);
+router.get('/users/:userId(\\d+)/edit', userController.edit);
+router.put('/users/:userId(\\d+)',      userController.update);
+router.delete('/users/:userId(\\d+)',   userController.destroy);
 
 
 // Routes for the resource /quizzes
