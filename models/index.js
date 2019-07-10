0e1f3a9ca7194895cf2107020e5f3ad03eb88a6b
Santiago Pavon <santiago@dit.upm.es>
2019-07-10 11:39:57 +0200

    Step 2.1.0: Quizzes Resource without Autoload.

---
+++ models/index.js
@@ -0,0 +1,17 @@
+const path = require('path');
+
+// Load ORM
+const Sequelize = require('sequelize');
+
+
+// Environment variable to define the URL of the data base to use.
+// To use SQLite data base:
+//    DATABASE_URL = sqlite:quiz.sqlite
+const url = process.env.DATABASE_URL || "sqlite:quiz.sqlite";
+
+const sequelize = new Sequelize(url);
+
+// Import the definition of the Quiz Table from quiz.js
+sequelize.import(path.join(__dirname, 'quiz'));
+
+module.exports = sequelize;
