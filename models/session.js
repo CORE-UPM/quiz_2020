d0509d69eeeb88aff1f6dd8e6c78194be4a41cfb
Santiago Pavon <santiago@dit.upm.es>
2019-07-10 13:00:00 +0200

    Step 2.4.2: Sessions with Sequelize.

---
+++ models/session.js
@@ -0,0 +1,28 @@
+
+const {Model} = require('sequelize');
+
+// Definition of the Session model:
+
+module.exports = (sequelize, DataTypes) => {
+
+    class Session extends Model {}
+
+    Session.init({
+            sid: {
+                type: DataTypes.STRING,
+                primaryKey: true
+            },
+            expires: {
+                type: DataTypes.DATE
+            },
+            data: {
+                type: DataTypes.STRING(50000)
+            }
+        }, {
+            sequelize,
+            modelName: "session"
+        }
+    );
+
+    return Session;
+};
