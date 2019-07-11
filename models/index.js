7f37b29455b28be7f87a44e4bd534963239a5a91
Santiago Pavon <santiago@dit.upm.es>
2019-07-11 19:28:46 +0200

    Step 8: Favourites.

--- models/index.js
+++ models/index.js
@@ -34,8 +34,25 @@ Attachment.hasOne(Quiz, {as: 'quiz', foreignKey: 'attachmentId'});
 Quiz.belongsTo(Attachment, {as: 'attachment', foreignKey: 'attachmentId'});
 
 // Relation 1-to-1 between User and Attachment
-User.belongsTo(Attachment, {as: "photo", foreignKey: 'photoId'});
 Attachment.hasOne(User, {as: 'user', foreignKey: 'photoId'});
+User.belongsTo(Attachment, {as: "photo", foreignKey: 'photoId'});
+
+// Relation N-to-N between Quiz and User:
+//    A User has many favourite quizzes.
+//    A quiz has many fans (the users who have marked it as favorite)
+Quiz.belongsToMany(User, {
+    as: 'fans',
+    through: 'Favourites',
+    foreignKey: 'quizId',
+    otherKey: 'userId'
+});
+
+User.belongsToMany(Quiz, {
+    as: 'favouriteQuizzes',
+    through: 'Favourites',
+    foreignKey: 'userId',
+    otherKey: 'quizId'
+});
 
 
 module.exports = sequelize;
