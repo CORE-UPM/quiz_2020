eeb0b4cef72a921c446e7be0ae583a810f6ccc7b
Santiago Pavon <santiago@dit.upm.es>
2019-07-12 10:39:13 +0200

    Step 8.1: API.

--- controllers/user.js
+++ controllers/user.js
@@ -1,8 +1,11 @@
+"use strict";
+
 const Sequelize = require("sequelize");
 const {models} = require("../models");
 const attHelper = require("../helpers/attachments");
 
 const paginate = require('../helpers/paginate').paginate;
+const authentication = require('../helpers/authentication');
 
 
 // Autoload the user with id equals to :userId
@@ -91,8 +94,11 @@ exports.create = async (req, res, next) => {
     let user = models.User.build({username, password});
 
     try {
-        // Save into the data base
-        user = await user.save({fields: ["username", "password", "salt"]});
+        // Create the token field:
+    user.token = authentication.createToken();
+
+    // Save into the data base
+        user = await user.save({fields: ["username", "token", "password", "salt"]});
         req.flash('success', 'User created successfully.');
 
         try {
@@ -263,3 +269,24 @@ exports.destroy = async (req, res, next) => {
         next(error)
     }
 };
+
+
+//-----------------------------------------------------------
+
+
+// PUT /users/:id/token
+// Create a saves a new user access token.
+exports.createToken = async (req, res, next) => {
+
+    req.load.user.token = authentication.createToken();
+
+    try {
+        const user = await req.load.user.save({fields: ["token"]});
+        req.flash('success', 'User Access Token created successfully.');
+        res.redirect('/users/' + user.id);
+    } catch (error) {
+        next(error);
+    }
+};
+
+//-----------------------------------------------------------
--- models/user.js
+++ models/user.js
@@ -1,3 +1,4 @@
+"use strict";
 
 const {Model} = require('sequelize');
 const crypt = require('../helpers/crypt');
@@ -13,27 +14,31 @@ module.exports = function (sequelize, DataTypes) {
     }
 
     User.init({
-        username: {
-            type: DataTypes.STRING,
-            unique: true,
-            validate: {notEmpty: {msg: "Username must not be empty."}}
-        },
-        password: {
-            type: DataTypes.STRING,
-            validate: {notEmpty: {msg: "Password must not be empty."}},
-            set(password) {
-                // Random String used as salt.
-                this.salt = Math.round((new Date().valueOf() * Math.random())) + '';
-                this.setDataValue('password', crypt.encryptPassword(password, this.salt));
+            username: {
+                type: DataTypes.STRING,
+                unique: true,
+                validate: {notEmpty: {msg: "Username must not be empty."}}
+            },
+            token: {
+                type: DataTypes.STRING,
+                validate: {notEmpty: {msg: "Token must not be empty."}}
+            },
+            password: {
+                type: DataTypes.STRING,
+                validate: {notEmpty: {msg: "Password must not be empty."}},
+                set(password) {
+                    // Random String used as salt.
+                    this.salt = Math.round((new Date().valueOf() * Math.random())) + '';
+                    this.setDataValue('password', crypt.encryptPassword(password, this.salt));
+                }
+            },
+            salt: {
+                type: DataTypes.STRING
+            },
+            isAdmin: {
+                type: DataTypes.BOOLEAN,
+                defaultValue: false
             }
-        },
-        salt: {
-            type: DataTypes.STRING
-        },
-        isAdmin: {
-            type: DataTypes.BOOLEAN,
-            defaultValue: false
-        }
         }, {
             sequelize
         }
