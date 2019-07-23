5f259b559440afd340fe8dcdb22c958efc8cbb27
Santiago Pavon <santiago@dit.upm.es>
2019-07-23 16:05:22 +0200

    Step 2.5: Restoration Routes.

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,37 @@ var router = express.Router();
 
 const quizController = require('../controllers/quiz');
 
+//-----------------------------------------------------------
+
+// History: Restoration routes.
+
+// Redirection to the saved restoration route.
+function redirectBack(req, res, next) {
+  const url = req.session.backURL || "/";
+  delete req.session.backURL;
+  res.redirect(url);
+}
+
+router.get('/goback', redirectBack);
+
+// Save the route that will be the current restoration route.
+function saveBack(req, res, next) {
+  req.session.backURL = req.url;
+  next();
+}
+
+// Restoration routes are GET routes that do not end in:
+//   /new, /edit, /play, /check, or /:id.
+router.get(
+    [
+      '/',
+      '/author',
+      '/quizzes'
+    ],
+    saveBack);
+
+//-----------------------------------------------------------
+
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index');
